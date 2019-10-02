299c93206249280ec6f9b8a03ae13e188e58ef1e
gtobio11 <gtobio11@gmail.com>
2019-10-02 11:12:51 +0900

    remove ranking overlapped code

--- src/component/Ranking.js
+++ src/component/Ranking.js
@@ -1,34 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Ranking.css'
+import "./Ranking.css";
+
+function getRankingPercent(amount) {
+  return ((amount / 25) * 100).toPrecision(4);
+}
 
 class Ranking extends Component {
-    state = {
-        teamList : ['blue', 'green', 'yellow', 'violet']
-    }
-    render() {
-        const { rankingData } = this.props;
+  state = {
+    teamList: ["blue", "green", "yellow", "violet"]
+  };
+  render() {
+    const { rankingData } = this.props;
 
-        return (
-            <div className="ranking-wrapper">
-                <div className="ranking-team">
-                    <span className={'ranking-team-circle ' + this.state.teamList[rankingData[0][0]]}>{rankingData[0][1]}개 부스 <br /> ({((rankingData[0][1] / 25)* 100).toPrecision(4)} %)</span>
-                    <span className="ranking-team-first">1</span>
-                </div>
-                <div className="ranking-team">
-                    <span className={'ranking-team-circle ' + this.state.teamList[rankingData[1][0]]}>{rankingData[1][1]}개 부스 <br /> ({((rankingData[1][1] / 25)* 100).toPrecision(4)} %)</span>
-                    <span className="ranking-team-second">2</span>
-                </div>
-                <div className="ranking-team">
-                    <span className={'ranking-team-circle ' + this.state.teamList[rankingData[2][0]]}>{rankingData[2][1]}개 부스 <br /> ({((rankingData[2][1] / 25)* 100).toPrecision(4)} %)</span>
-                    <span className="ranking-team-third">3</span>
-                </div>
-                <div className="ranking-team">
-                    <span className={'ranking-team-circle ' + this.state.teamList[rankingData[3][0]]}>{rankingData[3][1]}개 부스 <br /> ({((rankingData[3][1] / 25)*100).toPrecision(4)} %)</span>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="ranking-wrapper">
+        <div className="ranking-team-wrapper">
+          <span
+            className={
+              "ranking-team-circle " + this.state.teamList[rankingData[0][0]]
+            }
+          >
+            {rankingData[0][1]}개 부스 <br /> (
+            {getRankingPercent(rankingData[0][1])} %)
+          </span>
+          <span className="ranking-team ranking-team-first">1</span>
+        </div>
+        <div className="ranking-team-wrapper">
+          <span
+            className={
+              "ranking-team-circle " + this.state.teamList[rankingData[1][0]]
+            }
+          >
+            {rankingData[1][1]}개 부스 <br /> (
+            {getRankingPercent(rankingData[1][1])} %)
+          </span>
+          <span className="ranking-team ranking-team-second">2</span>
+        </div>
+        <div className="ranking-team-wrapper">
+          <span
+            className={
+              "ranking-team-circle " + this.state.teamList[rankingData[2][0]]
+            }
+          >
+            {rankingData[2][1]}개 부스 <br /> (
+            {getRankingPercent(rankingData[2][1])} %)
+          </span>
+          <span className="ranking-team ranking-team-third">3</span>
+        </div>
+        <div className="ranking-team-wrapper">
+          <span
+            className={
+              "ranking-team-circle " + this.state.teamList[rankingData[3][0]]
+            }
+          >
+            {rankingData[3][1]}개 부스 <br /> (
+            {getRankingPercent(rankingData[3][1])} %)
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Ranking;
