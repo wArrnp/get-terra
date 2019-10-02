24b5d057fe446a7f6331569bb39d78ac668dd973
gtobio11 <gtobio11@gmail.com>
2019-10-02 12:00:37 +0900

    make code clean

--- src/component/Show.js
+++ src/component/Show.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
-import './Show.css';
+import React, { Component } from "react";
+import "./Show.css";
+
+const colorList = ["blue", "green", "yellow", "violet"];
 
 class Show extends Component {
-    render() {
-        const { mapData } = this.props;
-        const mapList = mapData.map(data => {
-            let color;
-            if(data[1] === -1) color = 'gray';
-            else if(data[1] === 0) color = 'blue';
-            else if(data[1] === 1) color = 'green';
-            else if(data[1] === 2) color = 'yellow';
-            else if(data[1] === 3) color = 'violet';
-            return <span className={'show-content ' + color} key={data[0]}>{data[0]}</span>
-        })
-        return (
-            <div className="show-wrapper">
-                <div className="show-example-wrapper">
-                    <span className="show-example yellow"></span>
-                    <span className="show-example green"></span>
-                    <span className="show-example blue"></span>
-                    <span className="show-example violet"></span>
-                    <span className="show-example gray"></span>
-                </div>    
-                <div className="show-content-wrapper">
-                {mapList}
-                </div>  
-            </div>
-        );
-    }
+  render() {
+    const { mapData } = this.props;
+    const mapList = mapData.map(data => {
+      let color;
+      if (data[1] === -1) color = "gray";
+      else color = colorList[data[1]];
+      return (
+        <span className={"show-content " + color} key={data[0]}>
+          {data[0]}
+        </span>
+      );
+    });
+    return (
+      <div className="show-wrapper">
+        <div className="show-example-wrapper">
+          <span className="show-example yellow"></span>
+          <span className="show-example green"></span>
+          <span className="show-example blue"></span>
+          <span className="show-example violet"></span>
+          <span className="show-example gray"></span>
+        </div>
+        <div className="show-content-wrapper">{mapList}</div>
+      </div>
+    );
+  }
 }
 
 export default Show;
--- src/container/ShowContainer.js
+++ src/container/ShowContainer.js
@@ -1,53 +1,53 @@
-import React, { Component, Fragment } from 'react';
-import HeaderContainer from './HeaderContainer';
-import Show from '../component/Show'
-import Modal from '../component/Modal';
+import React, { Component } from "react";
+import HeaderContainer from "./HeaderContainer";
+import Show from "../component/Show";
+import Modal from "../component/Modal";
 
-import axios from 'axios';
+import axios from "axios";
 
 class ShowContainer extends Component {
+  state = {
+    mapData: [],
+    modal: false
+  };
 
-    state = {
-        mapData: [],
-        modal: false
-    }
+  interval = null;
 
-    interval = null;
+  getData = () => {
+    axios
+      .get("http://ec2.istruly.sexy:1234/map/web")
+      .then(response => {
+        this.setState({
+          mapData: response.data.map,
+          modal: false
+        });
+        console.log(response.data.map);
+      })
+      .catch(err => {
+        console.log(err);
+        if (err.response.status === 406) this.setState({ modal: true });
+      });
+  };
 
-    getData = () => {
-        axios.get('http://ec2.istruly.sexy:1234/map/web')
-            .then(response => {
-                this.setState({
-                    mapData: response.data.map,
-                    modal: false
-                });
-                console.log(response.data.map);
-            })
-            .catch(err => {
-                console.log(err);
-                if(err.response.status === 406) this.setState({modal: true});
-            })
-    }
+  componentDidMount() {
+    this.getData();
+    this.interval = setInterval(this.getData, 1000);
+  }
 
-    componentDidMount() {
-        this.getData()
-        this.interval = setInterval(this.getData, 1000);    
-    }
+  componentWillMount() {
+    clearInterval(this.interval);
+  }
 
-    componentWillMount() {
-        clearInterval(this.interval);
-    }
-
-    render() {
-        const { history, location } = this.props;
-        return (
-            <Fragment>
-                <HeaderContainer history={history} location={location}/>
-                <Show mapData={this.state.mapData} />
-                <Modal modalStatus={this.state.modal}/>
-            </Fragment>
-        );
-    }
+  render() {
+    const { history, location } = this.props;
+    return (
+      <>
+        <HeaderContainer history={history} location={location} />
+        <Show mapData={this.state.mapData} />
+        <Modal modalStatus={this.state.modal} />
+      </>
+    );
+  }
 }
 
 export default ShowContainer;
