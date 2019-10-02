9977d05079da48cc33003087837a8657eeba0ec8
gtobio11 <gtobio11@gmail.com>
2019-10-02 10:35:00 +0900

    Header style change

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 class Header extends Component {
-    render() {
-        const { onPaging, pathname } = this.props;
-        return (
-            <div className="header-wrapper">
-                <button className={pathname === '/show'? 'header-btn clicked': 'header-btn'} onClick={() => onPaging('/show')}>동아리 보기</button>
-                <button className={pathname === '/show'? 'header-btn': 'header-btn clicked'} onClick={() => onPaging('/ranking')}>현재 팀 순위</button>
-            </div>
-        );
-    }
+  render() {
+    const { onPaging, pathname } = this.props;
+    return (
+      <div className="header-wrapper">
+        <Link
+          className={
+            pathname === "/show" ? "header-link clicked" : "header-link"
+          }
+          to="/show"
+        >
+          동아리 보기
+        </Link>
+        <Link
+          className={
+            pathname === "/show" ? "header-link" : "header-link clicked"
+          }
+          to="/ranking"
+        >
+          현재 팀 순위
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Header;
